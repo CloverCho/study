2f2b29d4c516a3cc1f29f3d22c3d961202802607
CloverCho <43602849+CloverCho@users.noreply.github.com>
2019-11-21 16:50:59 +0900

    Create predictRain.py

---
+++ kaggle/weatherAUS/predictRain.py
@@ -0,0 +1,138 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv('weatherAUS.csv')
+print('Size of weather data frame is : ', df.shape)
+
+# Let us see how our data looks like!
+
+#print(df[0:5])
+
+# We see there are some columns with null values.
+# Before we start pre-processing, let's find out which of the columns have maximum null values
+print(df.count().sort_values())
+
+# As we can see the first four columns have less than 60% data, we can ignore these four columns
+# We don't need the location column because
+# we are going to find if it will rain in Australia(not location specific)
+# We are going to drop the date column too.
+# We need to remove RISK_MM because we want to predict 'RainTomorrow' and RISK_MM can leak some
+#  info to our model
+
+df = df.drop(columns=['Sunshine', 'Evaporation', 'Cloud3pm', 'Cloud9am', 'Location', 'RISK_MM',
+                      'Date'], axis=1)
+
+print(df.shape)
+
+# Let us get rid of all null values in df
+df = df.dropna(how="any")
+print(df.shape)
+
+#its time to remove the outliers in our data - we are using Z-score to detect and remove the outliers.
+from scipy import stats
+
+z = np.abs(stats.zscore(df._get_numeric_data()))
+print(z)
+df = df[(z<3).all(axis=1)]
+print(df.shape)
+
+# Lets deal with the categorical columns now
+# Simply change yes/no to 1/0 for RainToday and RainTomorrow
+df['RainToday'].replace({'No':0, 'Yes': 1}, inplace=True)
+df['RainTomorrow'].replace({'No':0, 'Yes':1}, inplace = True)
+
+# See unique values and convert them to int using pd.getDummies()
+categorical_columns = ['WindGustDir', 'WindDir3pm', 'WindDir9am']
+for col in categorical_columns:
+    print(np.unique(df[col]))
+
+# transform the categorical columns
+df = pd.get_dummies(df, columns=categorical_columns)
+print(df.iloc[4:9])
+
+# next step is to standardize our data - using MinMaxScalar
+from sklearn import preprocessing
+scaler = preprocessing.MinMaxScaler()
+scaler.fit(df)
+df = pd.DataFrame(scaler.transform(df), index=df.index, columns=df.columns)
+print(df.iloc[4:10])
+
+
+# Feature Selection
+
+# now that we are done with the pre-processing part, let's see which are the important features
+# for RainTomorrow!
+
+# Using SelectKBest to get the top features!
+from sklearn.feature_selection import SelectKBest, chi2
+X = df.loc[:, df.columns != 'RainTomorrow']
+y = df[['RainTomorrow']]
+selector = SelectKBest(chi2, k=3)
+selector.fit(X,y)
+X_new = selector.transform(X)
+print(X.columns[selector.get_support(indices=True)])    # top 3 columns
+
+# Let's get hold of the important features as assign them as X
+df = df[['Humidity3pm', 'Rainfall', 'RainToday', 'RainTomorrow']]
+X = df[['Humidity3pm']] # let's use only one feature Humidity3pm
+y = df[['RainTomorrow']]
+
+
+# Finding the best model
+
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import time
+
+t0 = time.time()
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25)
+clf_logreg = LogisticRegression(random_state=0)
+clf_logreg.fit(X_train, y_train)
+y_pred = clf_logreg.predict(X_test)
+score = accuracy_score(y_test, y_pred)
+print('Accuracy : ', score)
+print('Time taken : ', time.time()-t0)
+
+# Random Forest Classifier
+
+t0 = time.time()
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25)
+clf_rf = RandomForestClassifier(n_estimators=100, max_depth=4, random_state=0)
+clf_rf.fit(X_train, y_train)
+y_pred = clf_rf.predict(X_test)
+score = accuracy_score(y_test, y_pred)
+print('Accuracy : ', score)
+print('Time taken : ', time.time() - t0)
+
+
+# Decision Tree Classifier
+
+t0 = time.time()
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25)
+clf_dt = DecisionTreeClassifier(random_state=0)
+clf_dt.fit(X_train, y_train)
+y_pred = clf_dt.predict(X_test)
+score = accuracy_score(y_test, y_pred)
+print('Accuracy : ', score)
+print('Time taken : ', time.time()-t0)
+
+# Support Vector Machine
+
+t0 = time.time()
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.25)
+clf_svc = svm.SVC(kernel='linear')
+clf_svc.fit(X_train,y_train)
+y_pred = clf_svc.predict(X_test)
+score = accuracy_score(y_test, y_pred)
+print('Accuracy : ', score)
+print('Time taken : ', time.time() - t0)
+
+
